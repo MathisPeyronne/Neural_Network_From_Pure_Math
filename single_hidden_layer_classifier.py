639a5875dd76c969e5981bbd128322a75aa8e8a4
Mathis Peyronne <mathis.p@live.com>
2018-09-08 18:20:49 +0200

    Add files via upload

---
+++ single_hidden_layer_classifier.py
@@ -0,0 +1,180 @@
+import numpy as np
+
+X, Y = None  #load data
+
+# Visualize the data:
+plt.scatter(X[0, :], X[1, :], c=Y, s=40, cmap=plt.cm.Spectral);
+
+shape_X = X.shape
+shape_Y = Y.shape
+m = X.shape[1]  # training set size
+
+def sigmoid(x, derivative=False):
+  return 1/(1+np.exp(-x))
+
+def layer_sizes(X, Y):
+
+    n_x = X.shape[0] # size of input layer
+    n_h = 4
+    n_y = Y.shape[0] # size of output layer
+    return (n_x, n_h, n_y)
+
+
+def initialize_parameters(n_x, n_h, n_y):   
+    
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros(shape=(n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros(shape=(n_y, 1))
+    
+    assert (W1.shape == (n_h, n_x))
+    assert (b1.shape == (n_h, 1))
+    assert (W2.shape == (n_y, n_h))
+    assert (b2.shape == (n_y, 1))
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+
+def forward_propagation(X, parameters):
+
+    # Retrieve each parameter from the dictionary "parameters"
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    
+    # Implement Forward Propagation to calculate A2 (probabilities)
+    Z1 = np.dot(W1,X) + b1
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = sigmoid(Z2)
+    
+    assert(A2.shape == (1, X.shape[1]))
+    
+    cache = {"Z1": Z1,
+             "A1": A1,
+             "Z2": Z2,
+             "A2": A2}
+    
+    return A2, cache
+
+def compute_cost(A2, Y, parameters):
+
+    m = Y.shape[1] # number of example
+
+    # Compute the cross-entropy cost
+    cost = -(1.0/m) * np.sum(Y*np.log(A2) + (1-Y)*np.log(1-A2))
+    
+    cost = np.squeeze(cost)     # makes sure cost is the dimension we expect. 
+                                # E.g., turns [[17]] into 17 
+    assert(isinstance(cost, float))
+    
+    return cost
+
+def backward_propagation(parameters, cache, X, Y):
+
+    m = X.shape[1]
+    
+    # First, retrieve W1 and W2 from the dictionary "parameters".
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+        
+    # Retrieve also A1 and A2 from dictionary "cache".
+    A1 = cache["A1"]
+    A2 = cache["A2"]
+    
+    # Backward propagation: calculate dW1, db1, dW2, db2. 
+    dZ2 = A2 - Y
+    dW2 = np.dot(dZ2, A1.T)/m   #attention
+    db2 = np.sum(dZ2, axis = 1, keepdims = True)/m
+    dZ1 = np.multiply(np.dot(W2.T, dZ2), 1 - np.power(A1, 2))
+    dW1 = np.dot(dZ1, X.T)/m
+    db1 = np.sum(dZ1, axis=1, keepdims=True)/m
+    
+    grads = {"dW1": dW1,
+             "db1": db1,
+             "dW2": dW2,
+             "db2": db2}
+    
+    return grads
+
+def update_parameters(parameters, grads, learning_rate = 1.2):
+
+    # Retrieve each parameter from the dictionary "parameters"
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    
+    # Retrieve each gradient from the dictionary "grads"
+    dW1 = grads["dW1"]
+    db1 = grads["db1"]
+    dW2 = grads["dW2"]
+    db2 = grads["db2"]
+    
+    # Update rule for each parameter
+    W1 = W1 - learning_rate*dW1
+    b1 = b1 - learning_rate*db1
+    W2 = W2 - learning_rate*dW2
+    b2 = b2 - learning_rate*db2
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+
+def nn_model(X, Y, n_h, num_iterations = 10000, print_cost=False):
+    
+    np.random.seed(3)
+    n_x = layer_sizes(X, Y)[0]
+    n_y = layer_sizes(X, Y)[2]
+    
+    # Initialize parameters, then retrieve W1, b1, W2, b2. Inputs: "n_x, n_h, n_y". Outputs = "W1, b1, W2, b2, parameters".
+    parameters = initialize_parameters(n_x, n_h, n_y)
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    
+    # Loop (gradient descent)
+
+    for i in range(0, num_iterations):
+         
+        # Forward propagation. Inputs: "X, parameters". Outputs: "A2, cache".
+        A2, cache = forward_propagation(X, parameters)
+        
+        # Cost function. Inputs: "A2, Y, parameters". Outputs: "cost".
+        cost = compute_cost(A2, Y, parameters)
+ 
+        # Backpropagation. Inputs: "parameters, cache, X, Y". Outputs: "grads".
+        grads = backward_propagation(parameters, cache, X, Y)
+ 
+        # Gradient descent parameter update. Inputs: "parameters, grads". Outputs: "parameters".
+        parameters = update_parameters(parameters, grads)
+                
+        # Print the cost every 1000 iterations
+        if print_cost and i % 1000 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+
+    return parameters
+
+def predict(parameters, X):
+    
+    # Computes probabilities using forward propagation, and classifies to 0/1 using 0.5 as the threshold.
+    A2, cache = forward_propagation(X, parameters)
+    predictions = (A2 >= 0.5)
+    
+    return predictions
+
+# Build a model with a n_h-dimensional hidden layer
+parameters = nn_model(X, Y, n_h = 4, num_iterations = 10000, print_cost=True)
+
+# Print accuracy
+predictions = predict(parameters, X)
+print ('Accuracy: %d' % float((np.dot(Y,predictions.T) + np.dot(1-Y,1-predictions.T))/float(Y.size)*100) + '%')
